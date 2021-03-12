9260e603be01a6827496130a70e366f3d82f6b46
Thiago Seidel <thiagosseidel@gmail.com>
2021-03-12 12:44:09 -0300

    OrderStatus enum

--- src/main/java/com/thiagosseidel/course/entities/Order.java
+++ src/main/java/com/thiagosseidel/course/entities/Order.java
@@ -12,6 +12,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.thiagosseidel.course.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order")
@@ -26,6 +27,9 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	
+	Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -34,11 +38,12 @@ public class Order implements Serializable {
 		super();
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -56,6 +61,18 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		
+		if (orderStatus != null) {
+		
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
