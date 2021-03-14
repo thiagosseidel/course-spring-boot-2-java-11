00a8ed3ada7f093c8ade943d9d2fac934e652cbe
Thiago Seidel <thiagosseidel@gmail.com>
2021-03-13 23:39:10 -0300

    Subtotal & Total methods

--- src/main/java/com/thiagosseidel/course/entities/Order.java
+++ src/main/java/com/thiagosseidel/course/entities/Order.java
@@ -115,6 +115,18 @@ public class Order implements Serializable {
 		this.payment = payment;
 	}
 	
+	public Double getTotal() {
+		
+		double sum = 0.0;
+		
+		for (OrderItem oi : items) {
+			
+			sum += oi.getSubTotal();
+		}
+		
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
