81a16c16ecd38e22063043c2a00b53b50381e5f0
Thiago Seidel <thiagosseidel@gmail.com>
2021-03-14 13:18:42 -0300

    Exception handling - delete

---
+++ src/main/java/com/thiagosseidel/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,14 @@
+package com.thiagosseidel.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException() {
+		super();
+	}
+
+	public DatabaseException(String message) {
+		super(message);
+	}
+}
