a5890d60da0bbafb834af5656bcc876b6340b2ea
Thiago Seidel <thiagosseidel@gmail.com>
2021-03-14 11:39:44 -0300

    User update

--- src/main/java/com/thiagosseidel/course/services/UserService.java
+++ src/main/java/com/thiagosseidel/course/services/UserService.java
@@ -33,4 +33,20 @@ public class UserService {
 		
 		userRepository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		
+		User entity = userRepository.getOne(id);
+		
+		updateData(entity, obj);
+		
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
